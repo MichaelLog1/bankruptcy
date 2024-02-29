dc2e909a6dfe533dce9af5a1a1553c551c60aa5e
MichaelLog1 <michael.j.logsdon1@gmail.com>
2024-02-29 14:31:49 -0500

    restructutedbackend

---
+++ banruptcy/Frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import SideNav from './SideNav/SideNav'
+import Calendar from './Calendar/Calendar'
+
+function App() {
+  return (
+    <div>
+      <SideNav></SideNav>
+      <Calendar></Calendar>
+    </div>
+    
+  );
+}
+
+export default App;
