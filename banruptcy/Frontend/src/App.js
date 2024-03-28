bbea2ef7dc700b03f8068c30e8244c4debd83de2
MichaelLog1 <michael.j.logsdon1@gmail.com>
2024-03-28 14:20:09 -0400

    fixed merge issues

--- banruptcy/Frontend/src/App.js
+++ banruptcy/Frontend/src/App.js
@@ -1,37 +1,27 @@
 import React from "react";
 import './App.css';
-import SideNav from './SideNav/SideNav'
-import Calendar from './Calendar/Calendar'
+import TopNav from "./Components/TopNav";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import "./App.css"
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
-
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [data, setData] = React.useState(null);
-  const submitHandler = e => {
-    e.preventDefault()
-    axios.post('http://localhost:8080/signup', {username: username, password: password})
-    .then((data) => {
-      console.log(data)
-      setUsername('')
-      setPassword('')
-    })
-  }
-  //Code above basically declares the values that will be passed from frontend to backend - Josh 
-
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div>
-      <SideNav></SideNav>
-      <input id='example' type='text' value={username} onChange ={(e) => setUsername(e.target.value)}></input>
-      //Replace username with password and setUsername with setPassword and vice verse
-      <Calendar></Calendar>
+      <Router>
+        <TopNav></TopNav>
+            <Routes>
+                <Route exact path="/Home" element={<Home />} />
+                <Route path="/About" element={<About />} />
+                <Route path="/Signup" element={<Signup />}/>
+            </Routes>
+        </Router>
     </div>
   );
 }
