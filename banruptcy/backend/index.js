549c52877ab3751ebde750ed0bbe2c1986750c47
MichaelLog1 <michael.j.logsdon1@gmail.com>
2024-04-11 17:08:52 -0400

    Backend API's and Database

--- banruptcy/backend/index.js
+++ banruptcy/backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 // example API
 app.post("/Signup", (req, res) => {
@@ -8,34 +13,42 @@ app.post("/Signup", (req, res) => {
     const password = req.body.password;
     const email = req.body.email;
     // These vars now have the data from the signup form
-    console.log(username);
-    console.log(password);
-    console.log(email);
+    db.query(
+        "INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+        [username, password, email], 
+        (err, results) => {
+            console.log(err);
+        }
+    );
+});
 
+app.post("/Login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    db.query(
+        "SELECT * FROM users WHERE password = ? AND email = ?",
+        [password, email], 
+        (err, result) => {
+            if (err) {
+                res.send({err: err});
+            }
+            if (result) {
+                res.send(result);
+            } else {
+                res.send({ message: "Wrong email/pasword combination!" });
+            }
+        }
+    );
+});
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "Emily17Kirk$",
+    database: "LoginSystem",
 });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
-
-//Database
-app.post('/signup', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    db.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], (err, result) => {
-        if (err){
-            console.log(err)
-        }
-        else{
-            res.send({username: username})
-        }
-    })
-    /* username and password are values from the frontend (ex. input buttons)
-    that are sent through text that will be executed in MySQL
-    this command will add the value to the User table database
-    -Josh */
-})
-app.listen(8080, () => {
-    console.log(`Server listening on port 8080`);
-})
